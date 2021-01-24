e2287b79a26576e7500d447de5228c3c3b1ff842
Your Name <usubalieva13@gmail.com>
2021-01-24 14:21:48 +0600

    change error in url.py

--- todo/urls.py
+++ todo/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path("test2/", second),
     path("test3/", third)
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-    + static(settings.MEDIA_URL, document_root=settings._ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
