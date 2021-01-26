6a1d223bb3cc6a343dca6cc55fad6a52504e8417
Your Name <usubalieva13@gmail.com>
2021-01-26 20:38:40 +0600

    added todo marking

--- todo/urls.py
+++ todo/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path("test2/", second),
     path("add-todo/", add_todo, name="add-todo"),
     path("delete-todo/<id>/", delete_todo, name="delete-todo"),
+    path("mark-todo/<id>/", mark_todo, name="mark-todo"),
     path("book/", book, name="book"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
