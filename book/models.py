78e6346c260a3e3c0f62c9e6980d853361893e09
Your Name <usubalieva13@gmail.com>
2021-01-26 15:08:38 +0600

    changed name file templates

--- book/models.py
+++ book/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class BooK(models.Model):
     text = models.CharField(max_length=100)
     created_at = models.DateField(auto_now_add=True)
-    is_closed = models.BooleanField(default=False)
-    is_favorite = models.BooleanField(default=False)
+    
+
